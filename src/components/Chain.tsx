a8bfb0342c3c9cc231701180694b8088e767f6b5
Sergey Krigulson <krigulson@gmail.com>
2025-06-24 16:54:00 +0100

    Plan details

--- src/components/Chain.tsx
+++ src/components/Chain.tsx
@@ -1,12 +1,18 @@
 import {useEffect, useState} from 'preact/hooks'
 import type {WebSocketProvider} from 'ethers'
+import {ethers} from "ethers";
 import {CONTRACT_CONFIG} from "../config/chain.ts";
 import {CopyableAddress} from "./CopyAddress.tsx";
-import {ethers} from "ethers";
 import Pool from "../abi/Pool.json";
 
 type Props = {
     provider: WebSocketProvider | null
+}
+
+type TokenPlan = {
+    rentAmount: number,
+    epochDuration: number,
+    programEnd: number
 }
 
 export default function Chain({provider}: Props) {
@@ -15,6 +21,7 @@ export default function Chain({provider}: Props) {
     const [blockTime, setBlockTime] = useState<string>("")
     const [epochId, setEpochId] = useState<number>(0)
     const [epochEndTime, setEpochEndTime] = useState<number | null>(null)
+    const [plan, setPlan] = useState<TokenPlan | null>(null)
 
     const pool = new ethers.Contract(CONTRACT_CONFIG.poolAddress, Pool.abi, provider)
 
@@ -46,6 +53,9 @@ export default function Chain({provider}: Props) {
                 setEpochEndTime(Number(endTime));
                 setEpochId(epoch)
                 console.log("Epoch: %s, %s", epoch, endTime)
+                const plan: TokenPlan = await pool.getPlan()
+                console.log("Plan: ", plan)
+                setPlan(plan)
             } catch (e: unknown) {
                 if (e instanceof Error) {
                     console.error("Error calling contract:", e.message);
@@ -62,7 +72,7 @@ export default function Chain({provider}: Props) {
         }
 
 
-    }, [provider])
+    }, [provider, blockNumber])
 
     // @ts-ignore
     return (
@@ -78,6 +88,9 @@ export default function Chain({provider}: Props) {
                     <div>
                         Epoch: {epochId !== null ? epochId + " | " : 'loading... | '}
                         {epochEndTime ? new Date(epochEndTime * 1000).toISOString() : ""}
+                    </div>
+                    <div>
+                        Rent: {plan ? `${plan.rentAmount} | Epoch duration: ${plan.epochDuration} | Program end: ${new Date(Number(plan.programEnd) * 1000).toISOString()}` : 'loading...'}
                     </div>
                     <div><CopyableAddress label={"Token"}
                                           address={CONTRACT_CONFIG.realEstateTokenAddress}></CopyableAddress></div>
